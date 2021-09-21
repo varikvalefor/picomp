27f373415489be70f4c366992807534aaf74e054
Varik Valefor <varikvalefor@aol.com>
2021-09-21 18:55:23 -0400

    main.hs: Some semicolons are added.

--- src/main.hs
+++ src/main.hs
@@ -2,11 +2,11 @@ import Compression.Pi;
 import System.Environment;
 
 main :: IO ();
-main = getArgs >>= deicide
+main = getArgs >>= deicide;
 
-deicide :: [String] -> IO ()
+deicide :: [String] -> IO ();
 deicide a = case head a of
   "compress"   -> print $ compress $ stringToInteger $ a !! 1
   "decompress" -> putStrLn $ integerToString $
                   decompress (read $ a !! 1, read $ a !! 2)
-  _            -> error "An invalId command is entered."
+  _            -> error "An invalId command is entered.";
