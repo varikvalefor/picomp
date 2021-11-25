f8bf753f5c39f1822bfbe4ebdb908b89799943e5
Varik Valefor <varikvalefor@aol.com>
2021-11-25 11:49:56 -0500

    A function becomes a subfunction.

--- src/main.hs
+++ src/main.hs
@@ -2,11 +2,11 @@ import Compression.Pi;
 import System.Environment;
 
 main :: IO ();
-main = getArgs >>= deicide;
-
-deicide :: [String] -> IO ();
-deicide a = case head a of
-  "compress"   -> print $ compress $ stringToInteger $ a !! 1
-  "decompress" -> putStrLn $ integerToString $
-                  decompress (read $ a !! 1, read $ a !! 2)
-  _            -> error "An invalId command is entered.";
+main = getArgs >>= deicide
+  where
+  deicide :: [String] -> IO ()
+  deicide a = case head a of
+    "compress"   -> print $ compress $ stringToInteger $ a !! 1
+    "decompress" -> putStrLn $ integerToString $
+                    decompress (read $ a !! 1, read $ a !! 2)
+    _            -> error "An invalId command is entered.";
